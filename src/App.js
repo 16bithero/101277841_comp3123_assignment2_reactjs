f80d4777bca0c60ec58b3fdaba3c7e3f82bc0de5
16bithero <renzzi.adorador@gmail.com>
2022-12-04 04:12:10 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -3,27 +3,39 @@ import AddEmployee from './components/AddEmployee';
 import EmpView from './components/EmpView';
 import Update from './components/Update';
 import { BrowserRouter, Route, Routes, NavLink} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   return (
     <div className="App">
-      {/* <AddEmployee/>
-      <EmpView/>
-      <Update/> */}
-            <BrowserRouter>
-      <nav>
-          <ul>
-            <li><NavLink to='/add'>Add</NavLink></li>
-            <li><NavLink to='/view'>View</NavLink></li>
-            <li><NavLink to='/update'>Update</NavLink></li>
-          </ul>
-        </nav>
-        <Routes>
+      <BrowserRouter>
+      <Navbar  bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Employee Management App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/view">Home</Nav.Link>
+            <Nav.Link href="/add">Add</Nav.Link>
+            <NavDropdown title="Account" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+              
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.3">Log Out</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <Routes>
           <Route path='/add' element={<AddEmployee/>}/>
           <Route path='/view' element={<EmpView/>}/>
           <Route path='/update' element={<Update/>}/>
         </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
     </div>
   );
 }

285d1a9ed68055db8d525ba87ae9a093b0e4a59d
16bithero <renzzi.adorador@gmail.com>
2022-12-03 23:15:56 -0500

    first commit

---
+++ src/components/AddEmployee.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+export default function AddEmployee() {
+
+    const [data, setData] = useState('')
+
+  return (
+    <div>AddEmployee</div>
+    
+  )
+}

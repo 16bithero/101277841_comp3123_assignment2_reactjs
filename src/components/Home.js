f3838da24db390372a0fc0e1d15e69a94ef5935e
16bithero <renzzi.adorador@gmail.com>
2022-12-04 13:39:55 -0500

    added homepage

---
+++ src/components/Home.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { BrowserRouter, Route, Routes, NavLink, Link, useNavigate } from 'react-router-dom';
+
+export default function Home() {
+
+    const INITIAL_VALUE = {
+        username: "",
+        password: ""
+    }
+
+    let navigate = useNavigate();
+    const [data, setData] = useState('')
+
+    const addUser = async (event) => {
+        event.preventDefault()
+        const signup = {
+            username: data.username,
+            password: data.password,
+        }
+        axios.post('https://comp3123-assignment-101277841.herokuapp.com/user/add', signup)
+            .then(res => navigate('/view'))
+    }
+
+    const onValueChanged = (event) => {
+        event.preventDefault()
+        setData({ ...data, [event.target.name]: event.target.value })
+    }
+
+    return (
+        <>
+            <br />
+            <br />
+            <div className="divmid">
+                <div className="inside">
+                    <div className="divBody">
+                    <h2 style={{fontWeight:'bold'}}>Sign Up</h2>
+                        <section className="divbox">
+                            <Form>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Username</Form.Label>
+                                    <Form.Control type="text" name='username' placeholder="Enter First Name" value={data.username} onChange={event => onValueChanged(event)} />
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" name='password' placeholder="Enter Last Name" value={data.password} onChange={event => onValueChanged(event)} />
+                                </Form.Group>
+                                <Link to='/view'>
+                                    <Button variant="primary" size="lg" type='submit' onClick={addUser}>Sign Up</Button>
+                                </Link>
+                            </Form>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
